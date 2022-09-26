354f8c4b152af7d3f2b3d92ec802d853dc30a76a
“Nikhil <nikhil.shinde@rksv.in>
2022-09-26 20:46:57 +0530

    Added Spring boot boilerplate code

---
+++ src/main/java/com/personal/typingracer/TypingRacerApplication.java
@@ -0,0 +1,13 @@
+package com.personal.typingracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingRacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingRacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/typingracer/TypingRacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.typingracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingRacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
