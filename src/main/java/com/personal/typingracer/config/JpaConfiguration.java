a62648ef93782416b0bdccaa00320960407b98af
“Nikhil <nikhil.shinde@rksv.in>
2022-09-26 22:32:50 +0530

    Added code for mysql connection

---
+++ src/main/java/com/personal/typingracer/config/JpaConfiguration.java
@@ -0,0 +1,60 @@
+package com.personal.typingracer.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * @author nikhilshinde on 26/09/22
+ */
+@Configuration
+@EnableJpaRepositories("com.personal.typingracer.repository")
+public class JpaConfiguration {
+
+    @Autowired
+    private DataSourceProperties master;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(master.getDriverClassName());
+        dataSource.setUrl(master.getUrl());
+        dataSource.setUsername(master.getUsername());
+        dataSource.setPassword(master.getPassword());
+        return dataSource;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setGenerateDdl(true);
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean lemfb = new LocalContainerEntityManagerFactoryBean();
+        lemfb.setDataSource(dataSource());
+        lemfb.setJpaVendorAdapter(jpaVendorAdapter());
+        lemfb.setPackagesToScan("com.personal.typingracer.entity");
+        return lemfb;
+    }
+
+}
